1ef3489e7dd0c1e310374df4a66911f388382caf
Petr Tsymbarovich <petr@tsymbarovich.ru>
2021-06-30 16:34:20 +0300

    Add example of custom callback usage

--- example/rollup.config.custom-dir.js
+++ example/rollup.config.custom-dir.js
@@ -1,3 +1,5 @@
+import fs from 'fs'
+import path from 'path'
 import favicons from 'rollup-plugin-favicons'
 import html2 from 'rollup-plugin-html2'
 
@@ -5,7 +7,7 @@ import html2 from 'rollup-plugin-html2'
 export default {
   input: 'index.js',
   output: {
-    dir: 'dist',
+    dir: 'dist-custom-dir',
     format: 'es',
   },
   plugins: [
@@ -13,7 +15,13 @@ export default {
       source: 'icon.svg',
       configuration: {
         appName: process.env.npm_package_displayName,
+        path: '/public',
       },
+      callback({images, files}) {
+        const writeAsset = ({name, contents}) => fs.writeFileSync(path.resolve('public', name), contents)
+        images.forEach(writeAsset)
+        files.forEach(writeAsset)
+      }
     }),
     html2({
       template: 'index.html',
